5ed5dac06141929b20e0ecdfe40a93cc16d5fd8f
[dyad] <zaug.johnny@gmail.com>
2025-12-17 12:44:39 +0100

    [dyad] Ajustement du contrôle aérien et de la vitesse pour l'Étape 7 - wrote 1 file(s) + extra files edited outside of Dyad

--- public/games/learn/etape7/sketch.js
+++ public/games/learn/etape7/sketch.js
@@ -5,35 +5,26 @@ let floor;
 function setup() {
     createCanvas(800, 600);
     
-    // Physique dynamique (comme validé à l'étape 6)
     world.gravity.y = 25;
     
-    // --- JOUEUR ---
     player = new Sprite(100, 500, 30, 30);
     player.color = 'blue';
     player.collider = 'dynamic';
     player.rotationLock = true; 
     player.bounciness = 0;
-    player.friction = 0; // Gestion manuelle inertie
+    player.friction = 0;
 
-    // --- PLATEFORMES (Groupe) ---
     platforms = new Group();
     platforms.collider = 'static';
     platforms.color = 'green';
     
-    // Sol de départ
     floor = new platforms.Sprite(400, 580, 800, 40);
     floor.color = 'gray';
     
-    // --- GÉNÉRATION NIVEAU (Escalier) ---
-    // On crée 10 plateformes qui montent
     for(let i = 0; i < 10; i++) {
-        // Position X aléatoire mais accessible
-        // Position Y qui monte progressivement (-50px par marche)
-        let w = random(80, 150); // Largeur variable
+        let w = random(80, 150);
         let x = random(100, 700);
-        let y = 500 - (i * 55 + 50); // On commence au-dessus du sol
-        
+        let y = 500 - (i * 55 + 50);
         new platforms.Sprite(x, y, w, 20);
     }
 }
@@ -41,22 +32,33 @@ function setup() {
 function draw() {
     background(50);
     
-    // --- CONTROLE (Style Etape 6) ---
     let isGrounded = player.colliding(platforms);
 
-    if (isGrounded) {
-        let targetSpeed = 0;
-        if (kb.pressing('left')) targetSpeed = -6;
-        if (kb.pressing('right')) targetSpeed = 6;
-        player.vel.x = lerp(player.vel.x, targetSpeed, 0.2);
+    // --- INPUTS ---
+    let targetSpeed = 0;
+    
+    // Vitesse réduite : +/- 5 au lieu de +/- 6 ou plus
+    if (kb.pressing('left')) targetSpeed = -5;
+    if (kb.pressing('right')) targetSpeed = 5;
 
+    // --- PHYSIQUE DE DÉPLACEMENT ---
+    if (isGrounded) {
+        // AU SOL : Réactif (0.2)
+        // On change vite de direction
+        player.vel.x = lerp(player.vel.x, targetSpeed, 0.2);
+        
+        // SAUT
         if (kb.presses('space') || kb.presses('up')) {
             player.vel.y = -12;
         }
+    } else {
+        // EN L'AIR : Moins réactif (0.05)
+        // On peut influencer la trajectoire, mais c'est plus lent/lourd
+        // Cela permet de corriger un saut, mais pas de faire des zig-zags instantanés
+        player.vel.x = lerp(player.vel.x, targetSpeed, 0.05);
     }
     
-    // --- RESPAWN (Mort par chute) ---
-    // Si le joueur tombe sous l'écran
+    // --- RESPAWN ---
     if (player.y > height + 50) {
         resetPlayer();
     }
@@ -65,7 +67,6 @@ function draw() {
 }
 
 function resetPlayer() {
-    // Retour case départ
     player.pos = {x: 100, y: 500};
     player.vel = {x: 0, y: 0};
 }
